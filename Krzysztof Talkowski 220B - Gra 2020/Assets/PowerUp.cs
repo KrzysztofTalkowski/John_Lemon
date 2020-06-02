b2943906e2ab504a8f30f35f461e5e6073c731b7
Krzysztof Talkowski <k.talkowski@gmail.com>
2020-06-02 02:07:37 +0200

    Added magic lemons

---
+++ Krzysztof Talkowski 220B - Gra 2020/Assets/PowerUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public float multiplier = 0.7f;
+    public float duration = 2f;
+
+    public GameObject pickupEffect;
+    public GameObject smokeEffect;
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           StartCoroutine( Pickup(other) );
+
+        }
+    }
+
+    IEnumerator Pickup(Collider player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+        Instantiate(smokeEffect, transform.position, transform.rotation);
+
+
+        player.transform.localScale *= multiplier;
+
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        Component halo = GetComponent("Halo"); 
+        halo.GetType().GetProperty("enabled").SetValue(halo, false, null);
+
+
+        yield return new WaitForSeconds(duration);
+
+        player.transform.localScale /= multiplier;
+
+        Destroy(gameObject);
+    }
+}
